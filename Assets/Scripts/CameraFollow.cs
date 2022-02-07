ab8bc0580e97e2b48b87c879bd8662d63be26d55
roolies <rilie.s@comcast.net>
2022-02-06 22:16:19 -0500

    domainexpansion

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("PlayerTag").transform;
+        //we store current camera's position in variable temp - temporary position
+        Vector3 temp = transform.position;
+
+        //we set the camera's position x to be equal to the player's position x & y
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+        //we set back the camera's temp position to the camera's current position
+        transform.position = temp;
+    }
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    public int damage = 1;
+
+
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
